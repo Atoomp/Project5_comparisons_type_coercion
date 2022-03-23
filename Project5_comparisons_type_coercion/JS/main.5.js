d1850c8121e3496f2f7eccdb9c1d773e610c83a8
Atoomp <86169325+Atoomp@users.noreply.github.com>
2022-03-23 02:37:08 -0400

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.5.js
@@ -0,0 +1,17 @@
+document.write(typeof "Word");
+document.write(typeof 3);
+document.write(2E310);
+document.write(-2E310);
+document.write(10 == 10);
+document.write(3 == 11);
+X = 10;
+Y = 10;
+document.write(X === Y);
+X = 82;
+Y = "82";
+document.write(X == Y);
+A = "Magnus";
+B = "Magnus";
+document.write(A == B);
+document.write(6 > 2 && 9 > 3);
+document.write(2 > 6 || 9> 3);
